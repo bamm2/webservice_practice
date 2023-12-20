7d76b3b35d6c5497ed8f7d0780245c1cdb14a981
bamm2 <xxknu88@naver.com>
2023-12-20 23:27:47 +0900

    feat(PostsApiController): 삭제 컨트롤러

--- src/main/java/com/yubeom/study/springboot/web/controller/PostsApiController.java
+++ src/main/java/com/yubeom/study/springboot/web/controller/PostsApiController.java
@@ -5,6 +5,7 @@ import com.yubeom.study.springboot.web.dto.PostsSaveRequestDto;
 import com.yubeom.study.springboot.web.dto.PostsUpdateRequestDto;
 import com.yubeom.study.springboot.web.service.PostsService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,12 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return service.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        service.delete(id);
+        return id;
     }
 
 }

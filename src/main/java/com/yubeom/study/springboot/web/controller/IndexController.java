af94de66e6e52da78d250220e4a0cdcade6c88d7
bamm2 <xxknu88@naver.com>
2023-12-20 22:03:40 +0900

    feat(IndexController): 글 작성 페이지로 이동

--- src/main/java/com/yubeom/study/springboot/web/controller/IndexController.java
+++ src/main/java/com/yubeom/study/springboot/web/controller/IndexController.java
@@ -2,12 +2,23 @@ package com.yubeom.study.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
 }

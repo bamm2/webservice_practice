f7327ea48422c01b71a79f219faf3b13a808e517
bamm2 <xxknu88@naver.com>
2023-12-20 23:28:16 +0900

    feat(IndexController): 업데이트 컨트롤러 로직

--- src/main/java/com/yubeom/study/springboot/web/controller/IndexController.java
+++ src/main/java/com/yubeom/study/springboot/web/controller/IndexController.java
@@ -5,8 +5,8 @@ import com.yubeom.study.springboot.web.service.PostsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +18,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts",service.findAllDesc());
+        model.addAttribute("posts", service.findAllDesc());
         return "index";
     }
 
@@ -27,5 +27,10 @@ public class IndexController {
         return "posts-save";
     }
 
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post", service.findById(id));
+        return "posts-update";
+    }
 
 }

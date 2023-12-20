d31fe06c56c05a73888c7f05f26aa6497f763fbf
bamm2 <xxknu88@naver.com>
2023-12-20 22:42:06 +0900

    feat(PostsRepository): @Query 어노테이션으로 쿼리문 작성 연습

--- src/main/java/com/yubeom/study/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/yubeom/study/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,14 @@
 package com.yubeom.study.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // JpaRepository<Entity 클래스 , PK 타입>
     // @Repository 어노테이션 불필요
+
+    @Query("SELECT p FROM POSTS p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }

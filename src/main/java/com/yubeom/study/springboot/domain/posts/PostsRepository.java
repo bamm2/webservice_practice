00e9c0e361fda4e5432e381b00c1a93d2e6f25dd
bamm2 <xxknu88@naver.com>
2023-12-20 23:27:15 +0900

    fix(PostsRepository): 오타 수정

--- src/main/java/com/yubeom/study/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/yubeom/study/springboot/domain/posts/PostsRepository.java
@@ -9,6 +9,6 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     // JpaRepository<Entity 클래스 , PK 타입>
     // @Repository 어노테이션 불필요
 
-    @Query("SELECT p FROM POSTS p ORDER BY p.id DESC")
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 }

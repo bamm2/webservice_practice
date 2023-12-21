289f31655319c90144de369470a637648975a03a
bamm2 <xxknu88@naver.com>
2023-12-22 00:21:02 +0900

    feat(OAuthAttributes): naver 생성자

--- src/main/java/com/yubeom/study/springboot/Application.java
+++ src/main/java/com/yubeom/study/springboot/Application.java
@@ -2,9 +2,10 @@ package com.yubeom.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화 ,
+                    // Entity 가 하나 이상있어야 인식하지만 helloController 테스트는 엔티티가 없어 인식 못함 에러 발생
+                    // -> Config 클래스 생성 이동
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

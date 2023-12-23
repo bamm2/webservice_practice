aabd9f4f3ba23d60178b74cd4087a7ab3f1fff29
bamm2 <xxknu88@naver.com>
2023-12-24 00:36:56 +0900

    add: 배포 자동화

--- src/main/java/com/yubeom/study/springboot/Application.java
+++ src/main/java/com/yubeom/study/springboot/Application.java
@@ -2,6 +2,7 @@ package com.yubeom.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 //@EnableJpaAuditing // JPA Auditing 활성화 ,
                     // Entity 가 하나 이상있어야 인식하지만 helloController 테스트는 엔티티가 없어 인식 못함 에러 발생
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
